b13022a79736064b4c2f3e81e63890560fcda0d9
Hi-Imjaeyoung <imjy2757@naver.com>
2026-03-13 16:45:12 +0900

    feat: 멀티 모듈 뼈대 및 MySQL Master-Slave Replication 인프라 구축

---
+++ growup-common/src/main/java/org/example/domain/Member.java
@@ -0,0 +1,20 @@
+package org.example.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Entity
+@Builder
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+}
