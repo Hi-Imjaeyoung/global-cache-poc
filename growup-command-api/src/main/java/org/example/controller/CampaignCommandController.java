a3a5ae7c5a088afc68ff67c648c535e844e99cea
Hi-Imjaeyoung <imjy2757@naver.com>
2026-03-18 13:48:32 +0900

    fix: URL 맵핑 오류 수정.

--- growup-command-api/src/main/java/org/example/controller/CampaignCommandController.java
+++ growup-command-api/src/main/java/org/example/controller/CampaignCommandController.java
@@ -1,16 +1,18 @@
 package org.example.controller;
 
 import lombok.AllArgsConstructor;
-import org.example.CampaignDeleteFacade;
+import org.example.facade.CampaignDeleteFacade;
 import org.example.common.CommonResponse;
 import org.example.dto.CampaignDeleteDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/command/campaign")
+@RequestMapping("/api/command/campaign")
+@RestController
 @AllArgsConstructor
 public class CampaignCommandController {
 
