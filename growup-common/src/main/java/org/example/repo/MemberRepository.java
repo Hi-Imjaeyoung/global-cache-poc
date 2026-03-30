02bf78bb7dfd22f78df284e78930e2092bec32df
Hi-Imjaeyoung <imjy2757@naver.com>
2026-03-30 17:31:57 +0900

    fix: JPA 리턴 값 오류 해결.

--- growup-common/src/main/java/org/example/repo/MemberRepository.java
+++ growup-common/src/main/java/org/example/repo/MemberRepository.java
@@ -4,8 +4,10 @@ import org.example.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
-    Long findByEmail(String email);
+    Optional<Member> findByEmail(String email);;
 }
