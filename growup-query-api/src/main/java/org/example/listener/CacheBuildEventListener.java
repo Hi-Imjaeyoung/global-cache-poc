b5cb1f8f34fa0c51909d3271e8bbbffcccd7e797
Hi-Imjaeyoung <imjy2757@naver.com>
2026-04-05 15:31:59 +0900

    refactor: Redis 조회 작업 스레드 물리적 분리
    - 하나의 IO Bound에서 수행되던 작업 분리.

--- growup-query-api/src/main/java/org/example/listener/CacheBuildEventListener.java
+++ growup-query-api/src/main/java/org/example/listener/CacheBuildEventListener.java
@@ -1,10 +1,10 @@
 package org.example.listener;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.dto.AllCampaignTypeData;
-import org.example.exception.ErrorCode;
-import org.example.exception.GrouException;
 import org.example.config.CampaignRedisCacheManager;
 import org.example.service.KeywordService;
 import org.example.service.LazySegmentTreeService;
@@ -27,6 +27,7 @@ public class CacheBuildEventListener {
     private final CampaignRedisCacheManager campaignRedisCacheManager;
     private final Executor ioExecutor;  // ioExecutor 빈도 주입받아야 함!
     private final Executor cpuExecutor;
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleTreeBuildEvent(TreeBuildEvent event) {
@@ -36,33 +37,50 @@ public class CacheBuildEventListener {
             log.info("Tree is Already Building! email: {}, year: {}", email, year);
             return;
         }
-        CompletableFuture.supplyAsync(() -> {
-            AllCampaignTypeData[] savedData = campaignRedisCacheManager.getCachedTreeData(email, year);
-            if (savedData != null) {
-                log.debug("Redis 캐시 히트! 트리 인메모리 적재");
-                lazySegmentTreeService.saveBackupTreeData(email,year,savedData);
+        CompletableFuture.supplyAsync(()->{
+            try{
+                return campaignRedisCacheManager.getCachedTreeData(email, year);
+            }catch (Exception e){
+                log.warn("Redis L2 조회 실패 또는 서킷 Open 상태! DB 원본 조회로 이어갑니다.");
                 return null;
             }
-            log.debug("Redis 캐시 미스! DB 원본 조회 시작.");
-            LocalDate startDate = LocalDate.of(year, 1, 1);
-            LocalDate endDate = LocalDate.of(year, 12, 31);
-            return keywordService
-                    .getAllTypeOfCampaignAdCostSumAndAdSaleSumByPeriodAndEmailByDate(startDate, endDate, email);
-            }, CompletableFuture.delayedExecutor(1000, TimeUnit.MILLISECONDS, ioExecutor))
-            .thenAcceptAsync(data -> {
-                if (data == null) return;
-                log.debug("메모리 세그먼트 트리 빌드 시작...");
-                AllCampaignTypeData[] segTree = lazySegmentTreeService.startBuildTree(email, year, data);
-                log.debug("트리 빌드 완료!");
-                log.debug("현재 트리 메모리 크기는 {}",lazySegmentTreeService.getTreeMemory(email));
-                campaignRedisCacheManager.saveRawData(email,year,segTree);
-            }, cpuExecutor)
-            .whenComplete((result,ex) ->{
-                lazySegmentTreeService.removeTreeBuildingKey(email, year);
-                if (ex != null) {
-                    log.error("비동기 트리 빌드 파이프라인에서 치명적 에러 발생! [email: {}]", email, ex);
-                    throw new GrouException(ErrorCode.UNKNOWN_ERROR);
-                }
-            });
+        },CompletableFuture.delayedExecutor(1000,TimeUnit.MILLISECONDS,ioExecutor))
+                .thenApplyAsync(data -> {
+                    if(data != null){
+                        log.debug("Redis 캐시 Hit");
+                        lazySegmentTreeService.saveBackupTreeData(email,year,data);
+                        return null;
+                    }else{
+                        log.debug("Redis 캐시 미스! DB 원본 조회 시작.");
+                        CircuitBreaker.State state = circuitBreakerRegistry.circuitBreaker("redisCircuitBreaker").getState();
+                        if(state != CircuitBreaker.State.CLOSED){
+                            log.warn("Redis 서킷 Open 상태. DB 조회를 철회합니다. {}:{} L1 데이터 삭제 실시합니다.",email,year);
+                            lazySegmentTreeService.removeAllTreeData();
+                            return null;
+                        }
+                        LocalDate startDate = LocalDate.of(year, 1, 1);
+                        LocalDate endDate = LocalDate.of(year, 12, 31);
+                        return keywordService
+                                .getAllTypeOfCampaignAdCostSumAndAdSaleSumByPeriodAndEmailByDate(startDate, endDate, email);
+                    }
+                }, ioExecutor)
+                .thenApplyAsync(data -> {
+                    if (data == null) return null;
+                    log.debug("메모리 세그먼트 트리 빌드 시작...");
+                    AllCampaignTypeData[] segTree = lazySegmentTreeService.startBuildTree(email, year, data);
+                    log.debug("현재 트리 메모리 크기는 {}", lazySegmentTreeService.getTreeMemory(email));
+                    return segTree;
+                },cpuExecutor)
+                .thenAcceptAsync(segTree ->{
+                    if(segTree == null) return;
+                    campaignRedisCacheManager.saveSegTreeData(email, year, segTree);
+                    log.debug("Redis L2 캐시 저장 완료!");
+                },ioExecutor)
+                .whenComplete((result,ex)->{
+                    lazySegmentTreeService.removeTreeBuildingKey(email, year);
+                    if (ex != null) {
+                        log.error("비동기 트리 빌드 파이프라인에서 치명적 에러 발생! [email: {}]", email, ex);
+                    }
+                });
     }
 }
